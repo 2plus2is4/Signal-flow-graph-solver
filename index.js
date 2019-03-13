c8cf57c552733ea38adb22b2967dc1246906a2cd
Ezeny <ahmedezeny@gmail.com>
2019-03-13 14:04:52 +0200

    addNode nd Edges

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("hello!!!");
+var express = require("express");
+var app = express();
+
+app.use(express.static('Public'));
+app.use(express.static('node_modules'));
+
+var data ={
+	nodes: [{id: 1, label: 'someshit'},
+    		{id: 2, label: 'c2'},
+   			{id: 3, label: 'c3'}],
+	edges: [{from:1, to:1, arrows:'to', label: 'top', font: {align: 'top'}},
+			{from:2, to:3},
+			{from:1, to:3}],
+	options: {}
+}
+
+app.use("/api",function(req,res){
+	res.send(data).end();
+})
+app.listen(8080,function(){
+	console.log("your server started on port 8080");
+
+});
