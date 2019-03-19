b6f011250f86a0e5745ccae73c659c19e9ed2209
Ahmad Abdallah <ahmed_mosolino@yahoo.com>
2019-03-19 16:46:00 +0200

    forward paths on acyclic&cyclic graph

--- additional.js
+++ additional.js
@@ -10,11 +10,16 @@ g.setNode("x1", "x1");
 g.setNode("x2", "x2");
 g.setNode("x3", "x3");
 g.setNode("x4", "x4");
+g.setNode("x5", "x5");
+g.setNode("x6", "x6");
 //set edges
 g.setEdge("x1", "x2");
 g.setEdge("x2", "x3");
 g.setEdge("x3", "x4");
 g.setEdge("x2", "x4");
+g.setEdge("x1", "x6");
+g.setEdge("x6", "x5");
+g.setEdge("x5", "x1");
 //stack
 console.log(g.nodes());
 console.log(g.edges());
@@ -26,9 +31,12 @@ var paths = new Array();
  * @param id the targeted node
  */
 function forwardPaths(id) {
+    if(stack.includes(id)){
+        return;
+    }
     stack.push(id);
     if (g.successors(id).length == 0) {
-        var path = stack.copyWithin(stack.length, 0);
+        var path = stack.slice(0);
         paths.push(path);
     }else {
         for (var i = 0; i < g.successors(id).length; i++) {
