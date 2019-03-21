58d1399f65c56cadef55f75eee9974aa1d02bee8
Ezeny <ahmedezeny@gmail.com>
2019-03-21 15:53:09 +0200

    connect graphlib to vis

--- additional.js
+++ additional.js
@@ -1,19 +1,19 @@
 var imported = document.createElement('script');
 imported.src = 'node_modules/@dagrejs/graphlib/dist/graphlib.min.js';
-document.head.appendChild(imported);
+//document.head.appendChild(imported);
 //someshit
 Graph = graphlib.Graph;
 //creating a graph
 var g = new Graph();
 //set nodes
-g.setNode("x1", "x1");
-g.setNode("x2", "x2");
+g.setNode("f3d2bd1a-b2b0-4052-b0c0-5122479ba7bb",  -187.20000457763672, -140, "2");
+g.setNode("2", "x2");
 g.setNode("x3", "x3");
 g.setNode("x4", "x4");
 g.setNode("x5", "x5");
 g.setNode("x6", "x6");
 //set edges
-g.setEdge("x1", "x2");
+g.setEdge("867a70e8-4e73-49fe-9cd7-ca031d594e14","f3d2bd1a-b2b0-4052-b0c0-5122479ba7bb", "3");
 g.setEdge("x2", "x3");
 g.setEdge("x3", "x4");
 g.setEdge("x2", "x4");
@@ -23,6 +23,8 @@ g.setEdge("x5", "x1");
 //stack
 console.log(g.nodes());
 console.log(g.edges());
+//var g2 = graphlib.json.read(JSON.parse(str));
+console.log( graphlib.json.write(g));
 var stack = new Array();
 //all forward paths
 var paths = new Array();
--- index.js
+++ index.js
@@ -14,7 +14,6 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 
-
 var data ={
 	nodes: [],
 	edges: [],
